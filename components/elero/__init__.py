cc3cc6ab67af55c8d8a1b04fb9ca984a19f481e8
MayrK1303 <133364016+MayrK1303@users.noreply.github.com>
2026-08-18 19:05:42 +0200

    Add A-OK raw RF diagnostic mode

--- components/elero/__init__.py
+++ components/elero/__init__.py
@@ -15,6 +15,7 @@ CONF_FREQ0 = "freq0"
 CONF_FREQ1 = "freq1"
 CONF_FREQ2 = "freq2"
 CONF_RAW_DIAGNOSTIC = "raw_diagnostic"
+CONF_RAW_RSSI_THRESHOLD = "raw_rssi_threshold"
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -25,6 +26,7 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_FREQ1, default=0x71): cv.hex_int_range(min=0x0, max=0xff),
             cv.Optional(CONF_FREQ2, default=0x21): cv.hex_int_range(min=0x0, max=0xff),
             cv.Optional(CONF_RAW_DIAGNOSTIC, default=False): cv.boolean,
+            cv.Optional(CONF_RAW_RSSI_THRESHOLD, default=-90): cv.int_range(min=-120, max=-20),
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
@@ -43,3 +45,4 @@ async def to_code(config):
     cg.add(var.set_freq1(config[CONF_FREQ1]))
     cg.add(var.set_freq2(config[CONF_FREQ2]))
     cg.add(var.set_raw_diagnostic(config[CONF_RAW_DIAGNOSTIC]))
+    cg.add(var.set_raw_rssi_threshold(config[CONF_RAW_RSSI_THRESHOLD]))
